c198becd4341d90ccb91329a8e30710f4d1e589b
Chris Barrett <chris@walrus.cool>
2025-01-24 22:00:33 +1300

    Add keybindings for hexl

--- init.el
+++ init.el
@@ -633,6 +633,18 @@
 (use-package nix-ts-mode :ensure t
   :mode "\\.nix\\'")
 
+(use-package hexl
+  :general-config
+  (:states 'normal
+   :keymaps 'hexl-mode-map
+   "h" #'hexl-backward-char
+   "l" #'hexl-forward-char
+   "]]" #'hexl-end-of-1k-page
+   "[[" #'hexl-beginning-of-1k-page
+   "$" #'hexl-end-of-line
+   "^" #'hexl-beginning-of-line
+   "0" #'hexl-beginning-of-line))
+
 (use-package conf-mode
   ;; Unix configuration files
 
