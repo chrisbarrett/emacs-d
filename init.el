b8fb8f03a1ad7eac5b0af1e3d1ad4ebe03127c99
Chris Barrett <chris@walrus.cool>
2025-01-24 21:07:06 +1300

    Reveal context around point in org buffers in ediff

--- init.el
+++ init.el
@@ -264,7 +264,19 @@
   :custom
   (ediff-diff-options "-w")
   (ediff-split-window-function #'split-window-horizontally)
-  (ediff-window-setup-function #'ediff-setup-windows-plain))
+  (ediff-window-setup-function #'ediff-setup-windows-plain)
+  (ediff-show-clashes-only t)
+  :config
+  (with-eval-after-load 'org
+    (defun +ediff-reveal-org-content-around-hunk (&rest _)
+      (dolist (buf (list ediff-buffer-A ediff-buffer-B ediff-buffer-C))
+        (when (and buf (buffer-live-p buf))
+          (with-current-buffer buf
+            (when (derived-mode-p 'org-mode)
+              (org-reveal t))))))
+
+    (advice-add 'ediff-next-difference :after #'+ediff-reveal-org-content-around-hunk)
+    (advice-add 'ediff-previous-difference :after #'+ediff-reveal-org-content-around-hunk)))
 
 
 ;;; evil-mode
