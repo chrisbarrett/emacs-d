2efd575cc2285e8ebb67b2998828d91e6a5be08d
Chris Barrett <chris@walrus.cool>
2025-01-25 22:44:07 +1300

    :lipstick: defer-incrementally formatting

--- init.el
+++ init.el
@@ -1540,8 +1540,9 @@
 (use-package org-roam :ensure t
   :preface
   (setq org-roam-directory "~/org/roam")
+
   :defer-incrementally
-  '(ansi-color dash f rx seq magit-section emacsql)
+  ansi-color dash f rx seq magit-section emacsql
 
   :general-config
   (:states '(motion insert normal) :keymaps 'org-mode-map
