963c50d261e178eec29d0e7cd61c2ac078c4db32
Chris Barrett <chris@walrus.cool>
2025-01-27 10:48:07 +1300

    Run a hook when themes change

--- lisp/+theme.el
+++ lisp/+theme.el
@@ -3,6 +3,8 @@
 (defvar +theme-light nil)
 (defvar +theme-dark nil)
 
+(defvar +theme-changed-hook nil)
+
 (cl-defgeneric +system-theme-query (system-type))
 
 (cl-defmethod +system-theme-query ((_ (eql 'darwin)))
@@ -24,6 +26,7 @@
          (updated-theme (+theme-for-system-theme)))
     (dolist (theme custom-enabled-themes)
       (disable-theme theme))
-    (load-theme updated-theme t)))
+    (load-theme updated-theme t)
+    (run-hooks '+theme-changed-hook)))
 
 (provide '+theme)
