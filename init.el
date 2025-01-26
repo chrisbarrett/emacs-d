065656042a710dc7e1ad7d2ac1232acd3b2cdff9
Chris Barrett <chris@walrus.cool>
2025-01-27 12:02:23 +1300

    Inhibit spell-checking of org tags, which could be abbrevs

--- init.el
+++ init.el
@@ -746,7 +746,7 @@ Runs `+escape-hook'."
   (setq-hook! 'org-mode-hook
     spell-fu-faces-exclude '(org-meta-line org-link org-code org-block
                              org-block-begin-line org-block-end-line
-                             org-footnote)))
+                             org-footnote org-tag org-modern-tag)))
 
 (use-package flyspell-correct :ensure t
   ;; Provides a nicer command for working with spelling corrections.
