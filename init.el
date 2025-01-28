5ebfa32e706d81d5b802eb2eb6594ca7ee58e89b
Chris Barrett <chris@walrus.cool>
2025-01-28 18:42:09 +1300

    Set up doom-style transient hooks for deferred loading

--- init.el
+++ init.el
@@ -66,13 +66,39 @@
 
 
 
-;;; Extra UI lifecycle hooks
-;;
-;; These are cribbed from Doom.
+;;; Extra UI & session lifecycle hooks
 
-(defvar +switch-buffer-hook nil)
-(defvar +switch-frame-hook nil)
-(defvar +switch-window-hook nil)
+;; These hooks are cribbed from Doom--they're a great pattern for deferred
+;; loading.
+
+;; The first set are *transient* hooks; they are run only once in the Emacs
+;; session, the first time a particular action is performed by the user.
+
+(defvar +first-input-hook nil
+  "Transient hook before first user input.")
+
+(defvar +first-file-hook nil
+  "Transient hook before first interactively opened file.")
+
+(defvar +first-buffer-hook nil
+  "Transient hook before first interactively opened buffer.")
+
+;; These are set up to run just once by other hooks.
+
+(+run-hook-once '+first-buffer-hook '(+switch-buffer-hook find-file-hook))
+(+run-hook-once '+first-file-hook '(+switch-file-hook dired-initial-position-hook))
+(+run-hook-once '+first-input-hook '(pre-command-hook))
+
+;; The remaining hooks are executed routinely throughout the session.
+
+(defvar +switch-buffer-hook nil
+  "Hooks run after changing the current buffer.")
+
+(defvar +switch-frame-hook nil
+  "Hooks run after changing focused frame.")
+
+(defvar +switch-window-hook nil
+  "Hooks run after changing focused window.")
 
 (defun +run-switch-buffer-hooks-h (&optional _)
   (let ((gc-cons-threshold most-positive-fixnum)
