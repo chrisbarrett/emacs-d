bbccffdec06ba59e742454e506d31942aedd3c59
Chris Barrett <chris@walrus.cool>
2025-01-28 22:36:09 +1300

    Exclude archives from searches by update-agenda-files.sh

--- scripts/update-agenda-files.sh
+++ scripts/update-agenda-files.sh
@@ -7,8 +7,6 @@ org_dir=${ORG_DIRECTORY:-$HOME/org}
 cd "$org_dir"
 
 {
-    find . -maxdepth 1 -type f -name '*.org'
-    find archive -type f -name '*.org'
     rg --follow --files-with-matches '^(CLOCK:|[*]+ +(TODO|WAIT))' roam -g '!attach'
 
 } >"$org_dir"/org-agenda-files
