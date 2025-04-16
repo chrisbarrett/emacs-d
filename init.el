9db4f67fe5bd5dbbb1c38e82f107e796010128bb
Chris Barrett <chris@walrus.cool>
2025-04-16 15:50:07 +1200

    Use bufler instead of ibuffer

--- init.el
+++ init.el
@@ -276,8 +276,9 @@ Runs `+escape-hook'."
    "bb" '(bury-buffer :wk "bury")
    "bd" '(bury-buffer :wk "bury")
    "bD" '(kill-current-buffer :wk "kill")
-   "bl" '(ibuffer :wk "list")
+   "bl" '(bufler :wk "list")
    "bn" '(next-buffer :wk "next")
+   "bs" '(bufler-switch-buffer :wk "switch...")
    "bp" '(previous-buffer :wk "prev")
    "bc" (list
          (general-predicate-dispatch #'clone-indirect-buffer
@@ -1077,6 +1078,77 @@ With optional prefix arg CONTINUE-P, keep profiling."
   ;; Indentation behaviour.
   :custom
   (tab-first-completion 'word-or-paren-or-punct))
+
+(use-package bufler :ensure t
+  ;; A better buffer list than the default.
+  :hook +first-input-hook
+  :config
+  (bufler-defauto-group nix-store-path
+    (when-let* ((filename (or (buffer-file-name buffer)
+                              (when (buffer-base-buffer buffer)
+                                (buffer-file-name (buffer-base-buffer buffer)))))
+                (store-path (pcase (file-name-split filename)
+                              (`("" "nix" "store" ,store-path . ,_)
+                               store-path)))
+                (hash-delimiter (string-match "-" store-path))
+                ;; (hash (substring store-path 0 (1- hash-delimiter)))
+                (name (substring store-path (1+ hash-delimiter))))
+      (concat name)))
+
+  (setf bufler-groups
+        (bufler-defgroups
+          (group (auto-workspace))
+
+          (group
+           (group-or "*Help/Info*"
+                     (mode-match "*Help*" (rx bos "help-"))
+                     (mode-match "*Info*" (rx bos "info-"))))
+
+          (group
+           (dir user-emacs-directory))
+
+          (group
+           (dir org-directory)
+           (group
+            (auto-indirect)
+            (auto-file))
+           (group-not "*special*" (auto-file)))
+
+          (group
+           (auto-parent-project)
+           (group-not "special"
+                      (group-or "Non-file-backed and neither Dired nor Magit"
+                                (mode-match "Magit Status" (rx bos "magit-status"))
+                                (mode-match "Dired" (rx bos "dired-"))
+                                (auto-file))))
+
+          (group
+           (lambda (buf)
+             (when (and (buffer-file-name buf)
+                        (string-prefix-p "/nix/store" (buffer-file-name buf)))
+               "/nix/store"))
+           (bufler-group 'auto-nix-store-path))
+
+          (group
+           (group-not "*Special"
+                      (group-or "*Special*"
+                                (mode-match "Magit" (rx bos "magit-"))
+                                (mode-match "Forge" (rx bos "forge-"))
+                                (mode-match "Dired" (rx bos "dired"))
+                                (mode-match "grep" (rx bos "grep-"))
+                                (mode-match "compilation" (rx bos "compilation-"))
+                                (auto-file)))
+           (group
+            (name-match "**Special**"
+                        (rx bos "*" (or "Messages" "Warnings" "scratch" "Backtrace") "*")))
+           (group
+            (mode-match "*Magit* (non-status)" (rx bos "magit-"))
+            (auto-directory))
+
+           (auto-mode))
+
+          (auto-directory)
+          (auto-mode))))
 
 
 ;;; Open some files as read-only, e.g. vendored deps.
