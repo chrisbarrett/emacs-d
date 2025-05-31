3afe6ff23cdd0de9afa3e23e0ea5ccceea5479c8
Chris Barrett <chris@walrus.cool>
2025-05-31 19:14:01 +1200

    Refactor org-agenda custom commands

--- modules/mod-org-agenda.el
+++ modules/mod-org-agenda.el
@@ -26,85 +26,79 @@
 (setq org-archive-tag "ARCHIVED")
 (setq org-agenda-inhibit-startup nil)
 
+(setq org-agenda-todo-ignore-scheduled 'future)
+(setq org-agenda-span 'day)
+(setq org-agenda-window-setup 'only-window)
+(setq org-agenda-start-day nil)
+(setq org-agenda-include-diary nil)
+
+(setq org-agenda-insert-diary-extract-time nil)
+(setq org-agenda-show-inherited-tags nil)
+(setq org-agenda-skip-deadline-if-done t)
+(setq org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
+(setq org-agenda-skip-scheduled-if-done t)
+(setq org-agenda-start-on-weekday nil)
+(setq org-agenda-dim-blocked-tasks t)
+(setq org-agenda-sorting-strategy
+      '((agenda time-up habit-up priority-down category-up priority-down todo-state-up)
+        (todo priority-down category-up scheduled-up)
+        (tags priority-down category-up)
+        (search category-up)))
+
+(setq org-agenda-use-time-grid nil)
+(setq org-agenda-show-future-repeats nil)
+(setq org-agenda-ignore-properties '(effort appt))
+(setq org-agenda-clock-report-header "\nClocking")
+(setq org-agenda-clock-consistency-checks
+      '(:gap-ok-around ("12:20" "12:40" "4:00")
+        :max-duration "10:00"
+        :min-duration 0
+        :max-gap 0))
+
 (setq org-agenda-custom-commands
-      (let ((today
-             '(agenda ""
-               ((org-agenda-overriding-header "Agenda")
-                (org-agenda-use-time-grid t)
-                (org-agenda-clockreport-parameter-plist '(:compact t
-                                                          :link t
-                                                          :maxlevel 3
-                                                          :fileskip0 t
-                                                          :filetitle t))
-                (org-agenda-show-inherited-tags t)
-                (org-agenda-skip-function #'+agenda-view-skip-function)
-                (org-super-agenda-groups
-                 `((:name "Agenda" :time-grid t)
-                   (:name "Forming" :and (:habit t :regexp ,(rx "->")))
-                   (:name "French Study" :category "french")
-                   (:name "Cooking" :and (:habit t :tag "cooking"))
-                   (:name "Chores" :and (:habit t :tag "chore"))
-                   (:name "Habits" :habit t)
-                   (:name "Birthdays" :category "birthdays")
-                   (:name "Delegated" :todo "WAIT")
-                   (:name "Tickler" :tag "tickler"))))))
+      (let ((sections
+             '((agenda ""
+                ((org-agenda-overriding-header "Agenda")
+                 (org-agenda-use-time-grid t)
+                 (org-agenda-clockreport-parameter-plist '(:compact t
+                                                           :link t
+                                                           :maxlevel 3
+                                                           :fileskip0 t
+                                                           :filetitle t))
+                 (org-agenda-skip-function #'+agenda-view-skip-function)
+                 (org-super-agenda-groups
+                  `((:name "Agenda" :time-grid t)
+                    (:name "Forming" :and (:habit t :regexp ,(rx "->")))
+                    (:name "French Study" :category "french")
+                    (:name "Cooking" :and (:habit t :tag "cooking"))
+                    (:name "Chores" :and (:habit t :tag "chore"))
+                    (:name "Habits" :habit t)
+                    (:name "Birthdays" :category "birthdays")
+                    (:name "Delegated" :todo "WAIT")
+                    (:name "Tickler" :tag "tickler")))))
 
-            (next-actions
-             '(tags-todo "-tickler-inbox+TODO=\"TODO\""
-               ((org-agenda-overriding-header "Next Actions")
-                (org-agenda-dim-blocked-tasks 'invisible)
-                (org-agenda-skip-function #'+agenda-next-actions-skip-function))))
+               (tags-todo "-tickler-inbox+TODO=\"TODO\""
+                          ((org-agenda-overriding-header "Next Actions")
+                           (org-agenda-dim-blocked-tasks 'invisible)
+                           (org-agenda-skip-function #'+agenda-next-actions-skip-function)))
 
-            (inbox
-             '(tags-todo "+inbox+TODO=\"TODO\""
-               ((org-agenda-overriding-header "Inbox")
-                (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled)))))
+               (tags-todo "+inbox+TODO=\"TODO\""
+                          ((org-agenda-overriding-header "Inbox")
+                           (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled))))
 
-            (delegated
-             '(todo "WAIT"
-               ((org-agenda-overriding-header "Delegated")
-                (org-agenda-remove-tags nil)
-                (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled)))))
+               (todo "WAIT"
+                     ((org-agenda-overriding-header "Delegated")
+                      (org-agenda-remove-tags nil)
+                      (org-agenda-skip-function '(org-agenda-skip-entry-if 'scheduled))))
 
-            (projects
-             '(tags-todo "+TODO=\"PROJECT\""
-               ((org-agenda-overriding-header "Projects"))))
+               (tags-todo "+TODO=\"PROJECT\""
+                          ((org-agenda-overriding-header "Projects"))))))
 
-            (defaults
-             `((org-agenda-todo-ignore-scheduled 'future)
-               (org-agenda-span 'day)
-               (org-agenda-window-setup 'only-window)
-               (org-agenda-start-day nil)
-               (org-agenda-include-diary nil)
+        `(("p" "personal agenda" ,sections
+           ((org-agenda-tag-filter-preset '("-work" "-ignore"))))
 
-               (org-agenda-insert-diary-extract-time nil)
-               (org-agenda-show-inherited-tags nil)
-               (org-agenda-skip-deadline-if-done t)
-               (org-agenda-skip-deadline-prewarning-if-scheduled 'pre-scheduled)
-               (org-agenda-skip-scheduled-if-done t)
-               (org-agenda-start-on-weekday nil)
-               (org-agenda-dim-blocked-tasks t)
-               (org-agenda-sorting-strategy
-                '((agenda time-up habit-up priority-down category-up priority-down todo-state-up)
-                  (todo priority-down category-up scheduled-up)
-                  (tags priority-down category-up)
-                  (search category-up)))
-               (org-agenda-clock-report-header "\nClocking")
-               (org-agenda-use-time-grid nil)
-               (org-agenda-show-future-repeats nil)
-               (org-agenda-ignore-properties '(effort appt)))))
-
-        `(("p" "personal agenda" ,(list today next-actions inbox delegated projects)
-           (,@defaults
-            (org-agenda-tag-filter-preset '("-work" "-ignore"))))
-          ("w" "work agenda" ,(list today next-actions inbox delegated projects)
-           (,@defaults
-            (org-agenda-tag-filter-preset (list "-ignore" (format "+%s" (timekeep-work-tag))))
-            (org-agenda-clock-consistency-checks
-             '(:gap-ok-around ("12:20" "12:40" "4:00")
-               :max-duration "10:00"
-               :min-duration 0
-               :max-gap 0)))))))
+          ("w" "work agenda" ,sections
+           ((org-agenda-tag-filter-preset (list "-ignore" (format "+%s" (timekeep-work-tag)))))))))
 
 
 
