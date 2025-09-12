a219d88ad3e2384e2d0f3c183d65a0c780a5dbe5
Chris Barrett <chris@walrus.cool>
2025-09-12 16:41:43 +1200

    changes from chris-tracksuit2 on Fri Sep 12 16:41:43 NZST 2025

--- etc/transient/values.el
+++ etc/transient/values.el
@@ -1,3 +1,4 @@
 ((magit-branch-configure)
+ (magit-diff:magit-diff-mode "--ignore-space-change" "--no-ext-diff" "--stat")
  (magit-fetch "--prune" "--tags")
  (magit-log:magit-log-mode "-n256" "--follow" "--graph" "--decorate"))
