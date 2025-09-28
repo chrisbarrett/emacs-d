3e972d2a751732f974940451a277ddf7f62ba668
Chris Barrett <chris@walrus.cool>
2025-09-28 20:09:55 +1300

    Silence elpaca bootstrap warnings in emacs 31

--- init.el
+++ init.el
@@ -29,6 +29,7 @@
 
 ;; Suppress warning when loading Elpaca with latest Emacs.
 (add-to-list 'warning-suppress-types '(elpaca core \30.2))
+(add-to-list 'warning-suppress-types '(elpaca core \31.0.50))
 
 (unless (featurep 'elpaca)
   (load-file (file-name-concat user-emacs-directory "elpaca-bootstrap.el")))
