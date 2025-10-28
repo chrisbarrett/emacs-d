7c7a308ead22e71cf33a3ece6c9e7b5ecb051718
Chris Barrett <chris@walrus.cool>
2025-10-29 08:18:14 +1300

    Fix early transient load issue by wrapping in with-eval-after-load
    Elpaca whinges on startup that transient was loaded before it. This fix is a
    kludge, and there could be an underlying race condition.

--- modules/mod-tabs.el
+++ modules/mod-tabs.el
@@ -8,27 +8,32 @@
 ;;; Code:
 
 (require '+theme)
+(require 'cl-lib)
 (require 'color)
 (require 'tab-bar)
-(require 'transient)
 
-(transient-define-prefix +tabs-menu ()
-  "Transient menu for tab operations."
-  [["Navigation"
-    ("j" "Next tab" tab-bar-switch-to-next-tab)
-    ("l" "Next tab" tab-bar-switch-to-next-tab :if (lambda () nil))
-    ("k" "Previous tab" tab-bar-switch-to-prev-tab)
-    ("h" "Previous tab" tab-bar-switch-to-prev-tab :if (lambda () nil))
-    ("s" "Select tab by name" tab-bar-switch-to-tab)]
-   ["Management"
-    ("c" "Create new tab" tab-bar-new-tab)
-    ("d" "Close tab" tab-bar-close-tab)
-    ("x" "Close tab" tab-bar-close-tab :if (lambda () nil))
-    ("r" "Rename tab" tab-bar-rename-tab)
-    ("m" "Move tab" tab-bar-move-tab)]
-   ["Other"
-    ("u" "Undo close tab" tab-bar-undo-close-tab)
-    ("o" "Close other tabs" tab-bar-close-other-tabs)]])
+(cl-eval-when (compile)
+  (require 'transient))
+
+(with-eval-after-load 'transient
+  (transient-define-prefix +tabs-menu ()
+    "Transient menu for tab operations."
+    [["Navigation"
+      ("j" "Next tab" tab-bar-switch-to-next-tab)
+      ("l" "Next tab" tab-bar-switch-to-next-tab :if (lambda () nil))
+      ("k" "Previous tab" tab-bar-switch-to-prev-tab)
+      ("h" "Previous tab" tab-bar-switch-to-prev-tab :if (lambda () nil))
+      ("s" "Select tab by name" tab-bar-switch-to-tab)]
+     ["Management"
+      ("c" "Create new tab" tab-bar-new-tab)
+      ("d" "Close tab" tab-bar-close-tab)
+      ("x" "Close tab" tab-bar-close-tab :if (lambda () nil))
+      ("r" "Rename tab" tab-bar-rename-tab)
+      ("m" "Move tab" tab-bar-move-tab)]
+     ["Other"
+      ("u" "Undo close tab" tab-bar-undo-close-tab)
+      ("o" "Close other tabs" tab-bar-close-other-tabs)]]))
+
 
 ;; Left-pad the tab name.
 
