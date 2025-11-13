13eaec9380c021861e3df9144781d842934f9678
Chris Barrett <chris@walrus.cool>
2025-11-14 12:39:10 +1300

    Push with tags

--- etc/transient/values.el
+++ etc/transient/values.el
@@ -1,4 +1,5 @@
 ((magit-branch-configure)
  (magit-diff:magit-diff-mode "--ignore-space-change" "--no-ext-diff" "--stat")
  (magit-fetch "--prune" "--tags")
- (magit-log:magit-log-mode "-n256" "--follow" "--graph" "--decorate"))
+ (magit-log:magit-log-mode "-n256" "--follow" "--graph" "--decorate")
+ (magit-push "--follow-tags"))
