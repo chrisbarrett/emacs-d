f5c40de526cdb3d3846f02696e9249d2e4fda16d
Chris Barrett <chris@walrus.cool>
2025-12-30 12:50:36 +1300

    Pull help stuff out to an init file

--- init.el
+++ init.el
@@ -976,80 +976,6 @@ Runs `+escape-hook'."
     :after beframe))
 
 
-;;; Documentation systems
-
-(use-package help
-  ;; Defines the main help functionality for Emacs & Emacs-Lisp.
-  :hook (help-mode-hook . turn-on-visual-line-mode)
-  :custom
-  (help-window-select t)
-  :general
-  (:keymaps 'help-map
-            "h" nil ; view-hello-file: never intended, always annoying
-            "l" #'find-library
-            "c" #'describe-face
-            "P" #'describe-text-properties))
-
-(use-package help-mode
-  ;; Major-mode for help buffers.
-  :general-config
-  (:keymaps 'help-mode-map :states 'normal
-            "^" #'help-go-back
-            "M-n" #'forward-button
-            "M-p" #'backward-button
-            "C-n" #'forward-button
-            "C-p" #'backward-button))
-
-(use-package helpful :ensure t
-  ;; Extended help system, showing definitions etc. in the help buffer.
-  :general (:keymaps 'help-map
-                     "f" #'helpful-callable
-                     "v" #'helpful-variable
-                     "k" #'helpful-key))
-
-(use-package eldoc
-  ;; Display help hints in the echo area as you move around.
-  :config
-  ;; Teach eldoc to re-run after these commands.
-  (eldoc-add-command 'evil-normal-state
-                     'evil-insert
-                     'evil-change
-                     'evil-delete
-                     'evil-replace))
-
-(use-package info
-  ;; Emacs' built-in system for reading texinfo manuals.
-  :general
-  (:keymaps 'help-map "s" #'info-apropos)
-  :general-config
-  (:keymaps 'Info-mode-map :states 'normal "^" #'Info-up
-            "C-n" #'Info-forward-node
-            "C-p" #'Info-backward-node))
-
-(use-package man
-  ;; Built-in manpage reader.
-  :custom
-  ;; Tell man to use pop-to-buffer under the hood, which uses display-buffer and
-  ;; selects the window.
-  (Man-notify-method 'aggressive))
-
-(use-package imenu
-  ;; Emacs' built-in navigator for points of interest in a buffer.
-  :general-config
-  (:keymaps 'Info-mode-map [remap consult-imenu] #'Info-menu))
-
-(use-package rfc-mode :ensure t
-  ;; Interface for browsing RFC documents.
-  :general (:keymaps 'help-map "w" #'rfc-mode-browse)
-  :custom
-  (rfc-mode-directory "~/.cache/emacs/rfc-mode/rfcs/")
-  :config
-  ;; Fix the default face, which doesn't allow highlights (e.g. in vertico).
-  (custom-theme-set-faces 'user
-                          '(rfc-mode-browser-title-face ((t (:inherit bold)))))
-  (with-eval-after-load 'evil
-    (evil-set-initial-state 'rfc-mode 'motion)))
-
 
 ;;; Text & programming modes
 
