050eeed54b93825114c1d7c3565e09cfcb4b99f6
Chris Barrett <chris@walrus.cool>
2025-12-31 08:05:11 +1300

    Display full eglot diagnostic in echo area for fallthrough destination
    Detailed in the setup instructions for ocaml-eglot; seems reasonable to apply
    generally.
    
    https://github.com/tarides/ocaml-eglot?tab=readme-ov-file#configure-flymake

--- init/init-eglot.el
+++ init/init-eglot.el
@@ -4,20 +4,27 @@
 
 ;;; Code:
 
+(require '+corelib)
 
+;; Frontend for in-buffer error checking & navigation.
+;;
+;; See also: `next-error' and friends, which operate on compilation & grep
+;; results across any number of buffers.
 (use-package flymake
-  ;; Frontend for in-buffer error checking & navigation.
-  ;;
-  ;; c.f. `next-error' and friends, which operate on compilation & grep results
-  ;; across any number of buffers.
   :hook (prog-mode-hook . flymake-mode)
+
   :general-config (:keymaps 'flymake-mode-map
                             "M-n" #'flymake-goto-next-error
-                            "M-p" #'flymake-goto-prev-error))
+                            "M-p" #'flymake-goto-prev-error)
+
+  :config
+  ;; Display full diagnostic in echo area for fallthrough destination.
+  (alist-set! flymake-diagnostic-format-alist t '(origin code message)))
 
 
 ;; Emacs' built-in LSP integration.
 (use-package eglot
+
   :general-config
   (:keymaps 'eglot-mode-map
    :states '(insert normal)
@@ -25,12 +32,14 @@
   (:keymaps 'eglot-mode-map
    :states '(normal)
    "C-c C-r" #'eglot-rename)
+
   :custom
   (eglot-code-action-indicator "")
 
   ;; Make RET open markdown links in the eldoc buffer.
   ;;
   ;; https://github.com/joaotavora/eglot/discussions/1238
+
   :config
   (defun +eglot-open-link ()
     (interactive)
@@ -43,13 +52,15 @@
       (general-define-key :keymaps 'local :states 'motion "RET" #'+eglot-open-link)))
 
   ;; Prevent display of inlay hints
+
   (add-hook 'eglot-managed-mode-hook
             (defun +eglot-inlay-hints-off ()
               (eglot-inlay-hints-mode -1))))
 
 
 ;; Teach eglot to use lsp-booster for better performance.
-(use-package eglot-booster :ensure (eglot-booster :host github :repo "jdtsmith/eglot-booster")
+(use-package eglot-booster
+  :ensure (eglot-booster :host github :repo "jdtsmith/eglot-booster")
   :after eglot
   :demand t
   :config (eglot-booster-mode +1))
