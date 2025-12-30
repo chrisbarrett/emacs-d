da4632aeae92d1eb21190160cc82f8a344f9be12
Chris Barrett <chris@walrus.cool>
2025-12-31 09:12:37 +1300

    Set up major modes for opam and dune config files

--- init/init-ocaml.el
+++ init/init-ocaml.el
@@ -4,21 +4,43 @@
 
 ;;; Code:
 
+(require '+corelib)
+
 ;; Experimental OCaml major mode using tree-sitter.
 (use-package neocaml
   :ensure (neocaml :host github :repo "bbatsov/neocaml" :main "neocaml.el")
 
   :mode ("\\.ocamlinit\\'"))
 
-(use-package lisp-mode
-  :mode ("/dune-project\\'" . lisp-data-mode))
-
+;; Extend eglot to support extra functionality provided by the OCaml LSP.
 (use-package ocaml-eglot
   :ensure t
   :after neocaml
   :hook
   (neocaml-mode-hook . ocaml-eglot)
   (ocaml-eglot-hook . eglot-ensure))
+
+;; OPAM files use a complex config language, but `conf-colon-mode' is probably good
+;; enough as a basis for syntax highlighting.
+
+(add-to-list 'auto-mode-alist (cons (rx ".opam" eos) 'conf-colon-mode))
+
+(add-hook! 'conf-colon-mode-hook
+  (when (string-match-p "# This file is generated" (buffer-substring (point-min) (point-max)))
+    (read-only-mode +1)))
+
+;; Dune config files use an S-Expression language documented here:
+;;
+;; https://dune.readthedocs.io/en/stable/reference/lexical-conventions.html
+;;
+;; Define a custom mode for these.
+
+(define-derived-mode dune-config-mode lisp-data-mode "Dune Config"
+  (setq-local comment-add 0))
+
+(add-to-list 'auto-mode-alist (cons (rx "/dune" (? "-" (or "workspace" "project"))
+                                        eos)
+                                    #'dune-config-mode))
 
 (provide 'init-ocaml)
 
