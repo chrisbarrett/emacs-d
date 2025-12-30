a2426fc452fd8f8606993349a6e4952fbebf40c0
Chris Barrett <chris@walrus.cool>
2025-12-31 09:47:13 +1300

    Inline avy stuff into init-nav

--- init/init-nav.el
+++ init/init-nav.el
@@ -119,6 +119,42 @@
    [remap xref-go-forward] #'better-jumper-jump-forward))
 
 
+;; Jump to things or execute other actions by typing a few letters.
+(use-package avy :ensure t
+  :general ("M-g" #'avy-goto-char-timer)
+
+  :config
+  (defun +avy-action-change-move (pt)
+    "Delete the thing at PT and enter insert state."
+    (goto-char pt)
+    (avy-forward-item)
+    (kill-region pt (point))
+    (evil-insert-state)
+    (point))
+
+  (defun +avy-action-evil-lookup (pt)
+    "Look up the definition of thing at PT with evil."
+    (save-excursion
+      (goto-char pt)
+      (avy-forward-item)
+      (evil-lookup))
+    t)
+
+  :custom
+  ;; Customise the action keys to make actions a bit more vimmy.
+  (avy-dispatch-alist '((?x . avy-action-kill-stay)
+                        (?d . avy-action-kill-move)
+                        (?c . +avy-action-change-move)
+                        (?t . avy-action-teleport)
+                        (?v . avy-action-mark)
+                        (?y . avy-action-copy)
+                        (?p . avy-action-yank)
+                        (?P . avy-action-yank-line)
+                        (?i . avy-action-ispell)
+                        (?K . +avy-action-evil-lookup)
+                        (? . avy-action-zap-to-char))))
+
+
 (provide 'init-nav)
 
 ;;; init-nav.el ends here
