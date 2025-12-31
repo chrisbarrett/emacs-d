421963305fb17af49c9753cbe6644df7045fe502
Chris Barrett <chris@walrus.cool>
2025-12-31 14:05:59 +1300

    Teach direnv to propagate to ob; try to apply env earlier in mode setup

--- init/init-system.el
+++ init/init-system.el
@@ -51,7 +51,34 @@
   :if (executable-find "direnv")
   :custom
   (envrc-show-summary-in-minibuffer nil) ; very noisy.
-  )
+
+  ;; Execute org-babel source blocks in the host buffer's environment.
+
+  :functions envrc-propagate-environment
+  :config
+  (use-package ob
+    :functions org-babel-execute-src-block
+    :config
+    (advice-add #'org-babel-execute-src-block :around #'envrc-propagate-environment))
+
+  ;; Set up direnv *before* the major-mode's body executes. This ensures any
+  ;; hooks or advice in the mode setup sequence predictably have the direnv
+  ;; environment.
+
+  ;; See: https://github.com/doomemacs/doomemacs/blob/21682009b155c0b67ec47100e09cad3b298aa52f/modules/tools/direnv/config.el
+
+  :functions (envrc-global-mode-enable-in-buffer)
+  :defines envrc-global-mode
+
+  :preface
+  (defun +direnv-mode-setup-h ()
+    (if (not envrc-global-mode)
+        (remove-hook 'change-major-mode-after-body-hook #'envrc-global-mode-enable-in-buffer)
+      (remove-hook 'after-change-major-mode-hook #'envrc-global-mode-enable-in-buffer)
+      (add-hook 'change-major-mode-after-body-hook #'envrc-global-mode-enable-in-buffer 100)))
+
+  :config
+  (add-hook 'envrc-global-mode-hook #'+direnv-mode-setup-h))
 
 
 ;; Adds support for mise - https://mise.jdx.dev.
