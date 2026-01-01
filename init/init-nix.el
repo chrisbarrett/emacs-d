02ed761762c20d744b41981aa09fbf5a63e10014
Chris Barrett <chris@walrus.cool>
2026-01-02 09:46:25 +1300

    Use +dirlocals-set to mark nix store files as read-only

--- init/init-nix.el
+++ init/init-nix.el
@@ -6,21 +6,28 @@
 
 (require '+corelib)
 
+(+dirlocals-set "/nix/store/"
+  '((nil . ((mode . read-only)))))
+
+
 (use-package nix-ts-mode :ensure t
   :mode "\\.nix\\'"
   :hook (nix-ts-mode-local-vars-hook . eglot-ensure)
   :init
   (add-to-list 'auto-mode-alist (cons (rx "/flake.lock" eos) #'json-ts-mode)))
 
+
 (use-package +file-templates
   :config
   (+define-file-template (rx "flake.nix" eos) "flake.eld"))
+
 
 (use-package apheleia
   :defines apheleia-formatters
   :config
   (add-to-list 'apheleia-formatters '(nixpkgs-fmt "nixpkgs-fmt"))
   (setq-hook! 'nix-ts-mode-hook apheleia-formatter 'nixpkgs-fmt))
+
 
 (use-package project
   :config
