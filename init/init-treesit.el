bc10d7d66b9878e6c7eafc66363b11b8e3fa5e78
Chris Barrett <chris@walrus.cool>
2026-01-12 22:46:52 +1300

    Use treesitter's builtin grammar installation (Emacs 31)

--- init/init-treesit.el
+++ init/init-treesit.el
@@ -4,28 +4,10 @@
 
 ;;; Code:
 
-;; Automatic installation of treesitter grammars.
-(use-package treesit-auto :ensure t
-  :after-call +first-buffer-hook +first-file-hook
-  :commands global-treesit-auto-mode
+(use-package treesit
   :custom
-  (treesit-auto-install 'prompt)
-  :config
-  (treesit-auto-add-to-auto-mode-alist 'all)
-
-  ;; Cache the expensive grammar detection to avoid TTY performance issues
-  (defvar +treesit-auto--cached-remap-alist nil
-    "Cache for treesit-auto major mode remap alist.")
-
-  (define-advice treesit-auto--build-major-mode-remap-alist
-      (:around (orig-fn &rest args) cache-remap-alist)
-    "Cache the result of building the major mode remap alist to avoid expensive
-subprocess calls on every file open, especially problematic in TTY."
-    (or +treesit-auto--cached-remap-alist
-        (setq +treesit-auto--cached-remap-alist
-              (apply orig-fn args))))
-
-  (global-treesit-auto-mode +1))
+  (treesit-enabled-modes t) ; all
+  (treesit-auto-install-grammar 'always))
 
 
 ;; Use tree-sitter to mark syntactic elements.
