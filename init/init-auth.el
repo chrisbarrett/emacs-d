de6212b5e878bd5844a4a8493cab4b224e5ed744
Chris Barrett <chris@walrus.cool>
2026-01-23 08:45:41 +1300

    Use local version of emacs-auth-source-op when available

--- init/init-auth.el
+++ init/init-auth.el
@@ -2,20 +2,30 @@
 
 ;;; Commentary:
 
-;; Configure auth-source with 1Password backend via the `op' CLI.
-
-;;; Code:
-
 ;; Auth-source backend for 1Password via the `op' CLI.
 ;; Enables auth-source-search to retrieve credentials from 1Password for
 ;; packages like Forge, TRAMP, etc.
+
+;;; Code:
+
+(require 'elpaca)
+
+(let ((local-path (expand-file-name "~/src/chrisbarrett/emacs-auth-source-op")))
+
+  (if (file-directory-p local-path)
+      (add-to-list 'load-path local-path)
+    (elpaca (auth-source-op :host github :repo "chrisbarrett/emacs-auth-source-op"))))
+
+
 (use-package auth-source-op
-  :ensure (auth-source-op :host github :repo "chrisbarrett/emacs-auth-source-op")
   :after auth-source
   :demand t
+
   :custom
   (auth-sources '(1password))
   (auth-source-op-vaults '("Emacs"))
+
+  :functions (auth-source-op-enable)
   :config
   (auth-source-op-enable))
 
