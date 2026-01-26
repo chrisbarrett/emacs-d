2cc38bfa829236eba7702a8dc3244352e8a4afb3
Chris Barrett <chris@walrus.cool>
2026-01-26 21:32:15 +1300

    Add help module for module system migration
    Extracts help system configuration (helpful.el, eldoc, info, man,
    rfc-mode) into the new module structure. This is the third module
    migrated as part of Task 4.3.

---
+++ modules/help/init.el
@@ -0,0 +1,73 @@
+;;; init.el --- Help module initialization.  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; Configures help, helpful, eldoc, info, man, and rfc-mode.
+;; Provides vim-style keybindings and enhanced help buffers.
+
+;;; Code:
+
+;; Help: main help functionality for Emacs & Emacs-Lisp.
+(with-eval-after-load 'help
+  (setq help-window-select t)
+  (add-hook 'help-mode-hook #'turn-on-visual-line-mode)
+  (keymap-set help-map "h" nil)  ; remove view-hello-file
+  (keymap-set help-map "l" #'find-library)
+  (keymap-set help-map "c" #'describe-face)
+  (keymap-set help-map "P" #'describe-text-properties))
+
+;; help-mode: keybindings for help buffers
+(with-eval-after-load 'help-mode
+  (with-eval-after-load 'evil
+    (evil-define-key* 'normal help-mode-map
+      "^" #'help-go-back
+      (kbd "M-n") #'forward-button
+      (kbd "M-p") #'backward-button
+      (kbd "C-n") #'forward-button
+      (kbd "C-p") #'backward-button)))
+
+;; helpful: enhanced help buffers
+(with-eval-after-load 'helpful
+  (keymap-set help-map "f" #'helpful-callable)
+  (keymap-set help-map "v" #'helpful-variable)
+  (keymap-set help-map "k" #'helpful-key))
+
+;; eldoc: teach eldoc to re-run after evil state changes
+(with-eval-after-load 'eldoc
+  (eldoc-add-command 'evil-normal-state
+                     'evil-insert
+                     'evil-change
+                     'evil-delete
+                     'evil-replace))
+
+;; info: built-in texinfo manual reader
+(with-eval-after-load 'info
+  (keymap-set help-map "s" #'info-apropos)
+  (with-eval-after-load 'evil
+    (evil-define-key* 'normal Info-mode-map
+      "^" #'Info-up
+      (kbd "C-n") #'Info-forward-node
+      (kbd "C-p") #'Info-backward-node)))
+
+;; imenu: remap consult-imenu in Info-mode
+(with-eval-after-load 'imenu
+  (with-eval-after-load 'info
+    (keymap-set Info-mode-map "<remap> <consult-imenu>" #'Info-menu)))
+
+;; man: built-in manpage reader
+(with-eval-after-load 'man
+  (setq Man-notify-method 'aggressive))
+
+;; rfc-mode: RFC document browser
+(with-eval-after-load 'rfc-mode
+  (setq rfc-mode-directory "~/.cache/emacs/rfc-mode/rfcs/")
+  (keymap-set help-map "w" #'rfc-mode-browse)
+  ;; Fix browser title face to allow highlights
+  (custom-theme-set-faces 'user
+                          '(rfc-mode-browser-title-face ((t (:inherit bold)))))
+  (with-eval-after-load 'evil
+    (evil-set-initial-state 'rfc-mode 'motion)))
+
+(provide 'help-init)
+
+;;; init.el ends here
---
+++ modules/help/tests.el
@@ -0,0 +1,129 @@
+;;; tests.el --- Tests for help module.  -*- lexical-binding: t; -*-
+
+;;; Commentary:
+
+;; ERT tests for the help module, covering the spec's testable properties.
+
+;;; Code:
+
+(require 'ert)
+
+;; P1: `C-h f' calls `helpful-callable' (not `describe-function')
+(ert-deftest help-module-test-p1-help-f-binding ()
+  "P1: C-h f should be bound to helpful-callable."
+  (require 'help)
+  (require 'helpful)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "f") 'helpful-callable)))
+
+;; P2: `C-h v' calls `helpful-variable' (not `describe-variable')
+(ert-deftest help-module-test-p2-help-v-binding ()
+  "P2: C-h v should be bound to helpful-variable."
+  (require 'help)
+  (require 'helpful)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "v") 'helpful-variable)))
+
+;; P3: `C-h h' is unbound
+(ert-deftest help-module-test-p3-help-h-unbound ()
+  "P3: C-h h should be unbound (nil)."
+  (require 'help)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (null (keymap-lookup help-map "h"))))
+
+;; P4: `C-h l' calls `find-library'
+(ert-deftest help-module-test-p4-help-l-binding ()
+  "P4: C-h l should be bound to find-library."
+  (require 'help)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "l") 'find-library)))
+
+;; P5: `help-window-select' is t
+(ert-deftest help-module-test-p5-help-window-select ()
+  "P5: help-window-select should be t."
+  (require 'help)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq help-window-select t)))
+
+;; P6: `Man-notify-method' is `aggressive'
+(ert-deftest help-module-test-p6-man-notify-method ()
+  "P6: Man-notify-method should be aggressive."
+  (require 'man)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq Man-notify-method 'aggressive)))
+
+;; P7: Eldoc re-runs after evil state transitions
+(ert-deftest help-module-test-p7-eldoc-commands ()
+  "P7: Eldoc should re-run after evil state transitions."
+  (require 'eldoc)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (member 'evil-normal-state eldoc-message-commands))
+  (should (member 'evil-insert eldoc-message-commands))
+  (should (member 'evil-change eldoc-message-commands))
+  (should (member 'evil-delete eldoc-message-commands))
+  (should (member 'evil-replace eldoc-message-commands)))
+
+;; P8: In help-mode, `^' is bound to `help-go-back'
+(ert-deftest help-module-test-p8-help-mode-caret ()
+  "P8: In help-mode normal state, ^ should go back."
+  (require 'help-mode)
+  (require 'evil)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (evil-get-auxiliary-keymap help-mode-map 'normal t)
+              (evil-get-auxiliary-keymap help-mode-map 'normal)))
+  (let ((keymap (evil-get-auxiliary-keymap help-mode-map 'normal)))
+    (should (eq (keymap-lookup keymap "^") 'help-go-back))))
+
+;; P9: In Info-mode, `^' is bound to `Info-up'
+(ert-deftest help-module-test-p9-info-mode-caret ()
+  "P9: In Info-mode normal state, ^ should go up."
+  (require 'info)
+  (require 'evil)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (let ((keymap (evil-get-auxiliary-keymap Info-mode-map 'normal)))
+    (should (eq (keymap-lookup keymap "^") 'Info-up))))
+
+;; Additional tests for completeness
+
+(ert-deftest help-module-test-help-k-binding ()
+  "C-h k should be bound to helpful-key."
+  (require 'help)
+  (require 'helpful)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "k") 'helpful-key)))
+
+(ert-deftest help-module-test-help-c-binding ()
+  "C-h c should be bound to describe-face."
+  (require 'help)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "c") 'describe-face)))
+
+(ert-deftest help-module-test-help-P-binding ()
+  "C-h P should be bound to describe-text-properties."
+  (require 'help)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "P") 'describe-text-properties)))
+
+(ert-deftest help-module-test-help-s-binding ()
+  "C-h s should be bound to info-apropos."
+  (require 'help)
+  (require 'info)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "s") 'info-apropos)))
+
+(ert-deftest help-module-test-help-w-binding ()
+  "C-h w should be bound to rfc-mode-browse."
+  (require 'help)
+  (require 'rfc-mode)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (eq (keymap-lookup help-map "w") 'rfc-mode-browse)))
+
+(ert-deftest help-module-test-rfc-mode-directory ()
+  "rfc-mode-directory should be set to cache directory."
+  (require 'rfc-mode)
+  (load (expand-file-name "modules/help/init.el" user-emacs-directory) nil t)
+  (should (string= rfc-mode-directory "~/.cache/emacs/rfc-mode/rfcs/")))
+
+(provide 'help-tests)
+
+;;; tests.el ends here
