dee276f41ef90dcab7ddfef30f17817fe90ab4fa
Chris Barrett <chris@walrus.cool>
2026-01-27 21:30:35 +1300

    Restore missing theme init

--- modules/theme/init.el
+++ modules/theme/init.el
@@ -3,12 +3,16 @@
 ;;; Commentary:
 
 ;; Initializes theme configuration. Called after autoloads are registered.
-;;
-;; Note: The actual theme application is handled by init-theme.el using
-;; use-package to ensure the catppuccin-theme package is installed first.
-;; This module provides the library functions and face customizations.
 
 ;;; Code:
+
+(require 'theme-lib)
+
+(use-package catppuccin-theme
+  :demand t
+  :init
+  (setq +theme-dark 'catppuccin)
+  (+theme-update))
 
 ;; Custom face overrides for consistency across themes.
 (custom-theme-set-faces 'user
