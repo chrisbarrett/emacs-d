8804757dc17d3ccd3ec4a57d6b3117574fdbf39b
Chris Barrett <chris@walrus.cool>
2026-01-29 23:46:03 +1300

    Tidy up diff module

--- modules/diff/init.el
+++ modules/diff/init.el
@@ -8,32 +8,39 @@
 ;;; Code:
 
 ;; Diff: Unix diff file support.
-(with-eval-after-load 'diff-mode
-  (setq diff-default-read-only t)
-  (setq diff-advance-after-apply-hunk t)
-  (setq diff-font-lock-prettify t)
-  (setq diff-font-lock-syntax 'hunk-also))
+
+(use-package diff-mode
+  :custom
+  (diff-default-read-only t)
+  (diff-advance-after-apply-hunk t)
+  (diff-font-lock-prettify t)
+  (diff-font-lock-syntax 'hunk-also))
 
 ;; Ediff: Interactive file diff & merge UI.
-(with-eval-after-load 'ediff
-  (setq ediff-diff-options "-w")
-  (setq ediff-split-window-function #'split-window-horizontally)
-  (setq ediff-window-setup-function #'ediff-setup-windows-plain)
-  (setq ediff-show-clashes-only t))
+
+(use-package ediff
+  :custom
+  (ediff-diff-options "-w")
+  (ediff-split-window-function #'split-window-horizontally)
+  (ediff-window-setup-function #'ediff-setup-windows-plain)
+  (ediff-show-clashes-only t))
+
 
 ;; Org integration: reveal org headings when navigating ediff hunks.
-(defun +ad-ediff-reveal-org-content-around-hunk (&rest _)
-  "Reveal org heading around current hunk in ediff buffers."
-  (dolist (buf (list ediff-buffer-A ediff-buffer-B ediff-buffer-C))
-    (when (and buf (buffer-live-p buf))
-      (with-current-buffer buf
-        (when (derived-mode-p 'org-mode)
-          (org-reveal t))))))
 
-(with-eval-after-load 'ediff
+(use-package org
+  :after ediff
+  :autoload org-reveal
+  :preface
+  (defun +ad-ediff-reveal-org-content-around-hunk (&rest _)
+    "Reveal org heading around current hunk in ediff buffers."
+    (dolist (buf (list ediff-buffer-A ediff-buffer-B ediff-buffer-C))
+      (when (and buf (buffer-live-p buf))
+        (with-current-buffer buf
+          (when (derived-mode-p 'org-mode)
+            (org-reveal t))))))
+  :config
   (advice-add 'ediff-next-difference :after #'+ad-ediff-reveal-org-content-around-hunk)
   (advice-add 'ediff-previous-difference :after #'+ad-ediff-reveal-org-content-around-hunk))
-
-(provide 'diff-init)
 
 ;;; init.el ends here
