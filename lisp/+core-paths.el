d239cf3d2cfea962d86ddf1d251338d63aaca0f1
Chris Barrett <chris@walrus.cool>
2026-01-31 15:08:58 +1300

    Add lexical-binding cookie for core-paths

--- lisp/+core-paths.el
+++ lisp/+core-paths.el
@@ -1,6 +1,10 @@
+;;; core-paths.el -- paths in this emacs configuration -*- lexical-binding: t; -*-
+
 (defvar +lisp-dir (file-name-concat user-emacs-directory "lisp/"))
 (defvar +init-dir (file-name-concat user-emacs-directory "init/"))
 (defvar +config-dir (file-name-concat user-emacs-directory "config/"))
 (defvar +modules-directory (file-name-concat user-emacs-directory "modules/"))
 
 (provide '+core-paths)
+
+;;; core-paths.el ends here
